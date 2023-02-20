42f413968e11ece34b9ac118c0a0cadbb58942d7
rbulda50 <117113002+rbulda50@users.noreply.github.com>
2023-02-20 13:45:49 +0200

    updated
    fixed languages

--- src/js-modules/renderSingleCountryMarkup.js
+++ src/js-modules/renderSingleCountryMarkup.js
@@ -5,7 +5,7 @@ export default function renderSingleCountryMarkup(countries) {
     <img class="country-info__svg" src="${svg}" alt="${alt}">
   <li class="country-info__name">Назва країни: ${official}</li>
   <li class="country-info__capital">Столиця: ${capital}</li>
-  <li class="country-info__languages">Мова: ${Object.values(languages)}</li>
+  <li class="country-info__languages">Мова: ${Object.values(languages).join(', ')}</li>
   <li class="country-info__population">Населення: ${population}</li>
 </ul>`})
       .join('');
